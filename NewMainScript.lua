d68a66b19ac76216d61d4863fa88e680ea198603
skiddinglua <152652794+skiddinglua@users.noreply.github.com>
2023-12-01 15:17:20 -0500

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,12 +1,23 @@
-local gui = Instance.new('ScreenGui')
-local suc, res = pcall(function() gui.Parent = gethui and gethui() or game:GetService('CoreGui') end)
-if not suc then gui.Parent = game:GetService('Players').LocalPlayer.PlayerGui end
-local label = Instance.new('TextLabel')
-label.Font = Enum.Font.Gotham
-label.Text = 'Hi. My name is 7GrandDad and I\'m the developer of vape.\n\nVape will currently be on a indefinite hiatus for the future to come.\nUpdates will be provided at the 7GrandDadVape youtube channel, Thank you.'
-label.TextScaled = true
-label.Size = UDim2.new(1, 0, 1, 36)
-label.Position = UDim2.fromOffset(0, -36)
-label.BackgroundTransparency = 0.7
-label.Parent = gui
-game:GetService('Debris'):AddItem(gui, 60)
+		if isfolder("vape") then 
+			if ((not isfile("vape/commithash.txt")) or (readfile("vape/commithash.txt") ~= commit or commit == "main")) then
+				for i,v in pairs({"vape/Universal.lua", "vape/MainScript.lua", "vape/GuiLibrary.lua"}) do 
+					if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
+					if isfile(v) and ({readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.")})[1] == 1 then
+						delfile(v)
+					end 
+				end
+				if isfolder("vape/CustomModules") then 
+					for i,v in pairs(listfiles("vape/CustomModules")) do 
+						if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
+						if isfile(v) and ({readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.")})[1] == 1 then
+							delfile(v)
+						end 
+					end
+				end
+				if isfolder("vape/Libraries") then 
+					for i,v in pairs(listfiles("vape/Libraries")) do 
+						if isfile(v) and readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.") then
+						if isfile(v) and ({readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.")})[1] == 1 then
+							delfile(v)
+						end 
+					end
