a944c1b565b8e79b6b31949deedced51ea2abbcf
BlankedVoid <94419318+SystemXVoid@users.noreply.github.com>
2023-12-02 15:26:24 -0500

    commit

--- MainScript.lua
+++ MainScript.lua
@@ -134,6 +134,18 @@ local function displayErrorPopup(text, funclist)
 	setidentity(oldidentity)
 end
 
+local function getcommit()
+	local success, response = pcall(function()
+		return game:GetService("HttpService"):JSONDecode(game:HttpGet("https://api.github.com/repos/skiddinglua/NewVapeUnpatched4Roblox/commits"))
+	end)
+	local res = (success and response[1])
+	if res and response.documentation_url == nil and res.commit then 
+		local slash = res.commit.url:split("/")
+		return slash[#slash]
+	end
+	return "main"
+end
+
 local function getVapeFile(file, nolawl)
 	if not isfolder("vape") then 
 		makefolder("vape")
@@ -141,7 +153,7 @@ local function getVapeFile(file, nolawl)
 	local lawlwatermark = "-- lawl, credits to all of those who participated in fixing this project. https://discord.gg/Qx4cNHBvJq"
 	if not isfile("vape/"..file) or readfile("vape/"..file):find(lawlwatermark) == nil and not nolawl then 
 		local success, response = pcall(function()
-			return game:HttpGet("https://raw.githubusercontent.com/skiddinglua/NewVapeUnpatched4Roblox/main/"..file) 
+			return game:HttpGet("https://raw.githubusercontent.com/skiddinglua/NewVapeUnpatched4Roblox/"..getcommit().."/"..file) 
 		end)
 		if success and response ~= "404: Not Found" then 
 			response = (file:sub(#file - 4, #file) == ".lua" and lawlwatermark.."\n"..response or response)
