b6ca46c55a2add0796180fb8783ff89ca862a160
BlankedVoid <94419318+SystemXVoid@users.noreply.github.com>
2023-12-02 22:48:28 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,7 +5,7 @@ local function getVapeFile(file, nolawl)
     local lawlwatermark = "-- lawl, credits to all of those who participated in fixing this project. https://discord.gg/Qx4cNHBvJq"
     if not isfile("vape/"..file) or readfile("vape/"..file):find(lawlwatermark) == nil and not nolawl or nolawl and readfile("vape/"..file):find(lawlwatermark) == nil then 
         local success, response = pcall(function()
-            return game:HttpGet("https://raw.githubusercontent.com/skiddinglua/NewVapeUnpatched4Roblox/main/"..file) 
+            return game:HttpGet("https://raw.githubusercontent.com/skiddinglua/NewVapeUnpatched4Roblox/main/"..file, true) 
         end)
         if success and response ~= "404: Not Found" then 
             response = (file:split(".")[#file:split(".")] == "lua" and lawlwatermark.."\n"..response or response)
