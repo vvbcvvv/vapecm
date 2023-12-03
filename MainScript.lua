eefb0b26f7357960f1ac64cff34413ca06fb7a9f
BlankedVoid <94419318+SystemXVoid@users.noreply.github.com>
2023-12-02 22:33:52 -0500

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -137,24 +137,24 @@ local function displayErrorPopup(text, funclist)
 end
 
 local function getVapeFile(file, nolawl)
-	if not isfolder("vape") then 
-		makefolder("vape")
-	end
-	local lawlwatermark = "-- lawl, credits to all of those who participated in fixing this project. https://discord.gg/Qx4cNHBvJq"
-	if not isfile("vape/"..file) or readfile("vape/"..file):find(lawlwatermark) == nil and not nolawl or nolawl then 
-		local success, response = pcall(function()
-			return game:HttpGet("https://raw.githubusercontent.com/skiddinglua/NewVapeUnpatched4Roblox/main/"..file) 
-		end)
-		if success and response ~= "404: Not Found" then 
-			response = (file:sub(#file - 4, #file) == ".lua" and lawlwatermark.."\n"..response or response)
-			writefile("vape/"..file, response)
-			return response
-		else
-			task.spawn(error, "Vape Unpatched - Failed to download "..file.." | HTTP 404")
-			return task.wait(9e9)
-		end 
-	end
-	return isfile("vape/"..file) and readfile("vape/"..file) or error("Vape Unpatched - Failed to read "..file)
+    if not isfolder("vape") then 
+        makefolder("vape")
+    end
+    local lawlwatermark = "-- lawl, credits to all of those who participated in fixing this project. https://discord.gg/Qx4cNHBvJq"
+    if not isfile("vape/"..file) or readfile("vape/"..file):find(lawlwatermark) == nil and not nolawl or nolawl and readfile("vape/"..file):find(lawlwatermark) == nil then 
+        local success, response = pcall(function()
+            return game:HttpGet("https://raw.githubusercontent.com/skiddinglua/NewVapeUnpatched4Roblox/main/"..file) 
+        end)
+        if success and response ~= "404: Not Found" then 
+            response = (file:split(".")[#file:split(".")] == "lua" and lawlwatermark.."\n"..response or response)
+            writefile("vape/"..file, response)
+            return response
+        else
+            error("Vape Unpatched - Failed to download "..file.." | HTTP 404")
+            return task.wait(9e9)
+        end 
+    end
+    return isfile("vape/"..file) and readfile("vape/"..file) or task.wait(9e9)
 end
 
 local function downloadVapeAsset(path)
