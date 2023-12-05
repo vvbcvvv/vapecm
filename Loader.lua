ec11c230c17ff363ba5822c9ba2a2f9d9f7b7bda
scrxpted <80987982+supercellgamer@users.noreply.github.com>
2023-12-05 08:03:18 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,9 @@ local commit = 'main'
 for i,v in pairs(game:HttpGet('https://github.com/skiddinglua/NewVapeUnpatched4Roblox'):split('\n')) do 
 	if v:find('commit') and v:find('fragment') then 
 		local str = v:split('/')[5]
-		commit = str:sub(0, str:find('\'') - 1)
+		if str:find('\'') then
+			commit = str:sub(0, str:find('\'') - 1)
+		end
 		break
 	end
 end
