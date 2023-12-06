97c42a796078b957adc6a0165ea9daa8c2cbfb3a
scrxpted <80987982+supercellgamer@users.noreply.github.com>
2023-12-06 16:42:28 -0600

    ...

--- MainScript.lua
+++ MainScript.lua
@@ -192,7 +192,6 @@ GuiLibrary = loadstring(vapeGithubRequest("GuiLibrary.lua"))()
 shared.GuiLibrary = GuiLibrary
 
 local saveSettingsLoop = coroutine.create(function()
-	if inputService.TouchEnabled then return end
 	repeat
 		GuiLibrary.SaveSettings()
         task.wait(10)
