27b0453ddce3ff5cb31453cf845b43a45dd97a64
vvbcvvv <153875705+vvbcvvv@users.noreply.github.com>
2023-12-15 01:50:07 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 local commit = 'main'
-for i,v in pairs(game:HttpGet('https://github.com/skiddinglua/vapecm'):split('\n')) do 
+for i,v in pairs(game:HttpGet('https://github.com/vvbcvvv/vapecm'):split('\n')) do 
 	if v:find('commit') and v:find('fragment') then 
 		local str = v:split('/')[5]
 		commit = str:sub(0, str:find('"') - 1)
@@ -7,4 +7,4 @@ for i,v in pairs(game:HttpGet('https://github.com/skiddinglua/vapecm'):split('\n
 	end
 end
 print(commit)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/skiddinglua/vapecm/'..commit..'/Backend.lua', true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vvbcvvv/vapecm/'..commit..'/Backend.lua', true))()
